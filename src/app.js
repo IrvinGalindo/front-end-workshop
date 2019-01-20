29feb6fe15b866ac670574451ee90efb12ab0406
Irvin Galindo <irvin_its@outlook.es>
2019-01-20 00:26:23 -0600

    feat(footer): add app file

--- src/app.js
+++ src/app.js
@@ -4,19 +4,8 @@ import ReactDOM from 'react-dom';
 import './scss/app';
 import 'evil-icons/assets/evil-icons.js';
 
-import Header from './components/header';
 import Footer from './components/footer';
-import Cover from './components/cover';
-import ListPets from './components/list-pets';
 
-const App = () =>   (<React.Fragment>
-                        <Header />
-                        <main>
-                            <Cover />
-                            <ListPets />
-                        </main>
-                        <Footer />
-                    </React.Fragment>
-                    )
+const App = () =>   <Footer />
 
 ReactDOM.render(<App />, document.getElementById('root'));
